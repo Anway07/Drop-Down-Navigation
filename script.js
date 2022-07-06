f17077e0f3245b2f1cf7f308dafbd8f075f2e195
Hypertext_Assassian <106700249+Anway07@users.noreply.github.com>
2022-07-06 17:49:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let Menutog = document.querySelector('.Menutog');
+let navigation = document.querySelector('.navigation');
+
+Menutog.onclick  = function(){
+  navigation.classList.toggle('active')
+}
